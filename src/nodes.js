f8a96758b043d61d3b6a51e510445ef8260a5606
david-jughead <makutososa300@gmail.com>
2022-10-14 22:58:43 -0400

    secciones de navegacion

---
+++ src/nodes.js
@@ -0,0 +1,27 @@
+// Sections
+const headerSection = document.querySelector('#header');
+const trendingPreviewSection = document.querySelector('#categoriesPreview');
+const categoriesPreviewSection = document.querySelector('#categoriesPreview');
+const generiSection = document.querySelector('#genericList');
+const movieDetailSection = document.querySelector('#movieDetail');
+
+// List & Containers
+const searchFrom = document.querySelector('#searchFrom');
+const trendingMoviesPreviewList = document.querySelector('.trendingPreview-movieList');
+const categoriesPreviewList = document.querySelector('.categoriesPreview-list');
+const movieDetailCategoriesList = document.querySelector('#movieDetail .categories-list');
+const relatedMoviesContainer = document.querySelector('.relaredMovies-scrollContainer');
+
+// Elements
+const headerTitle = document.querySelector('.header-title');
+const arrowBtn = document.querySelector('.header-arrow');
+const headerCategoryTitle = document.querySelector('.header-title--categoryView');
+
+const searchFormInput = document.querySelector('#searchForm imput');
+const searchFromBtn = document.querySelector('#searchBtn');
+
+const trendingBtn = document.querySelector('.trendingPreview-btn');
+
+const movieDetailtitle = document.querySelector('.movieDetail-title');
+const movieDetailDescription = document.querySelector('.movieDetail-description');
+const movieDetailScore = document.querySelector('.movieDetail-score');
