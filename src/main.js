339aa3c91e3372e59a07be8da11e4f6acedf52ff
david-jughead <makutososa300@gmail.com>
2022-10-22 14:59:55 -0400

    save movies into local storage

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// Data
+
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   headers: {
@@ -8,6 +10,30 @@ const api = axios.create({
   },
 });
 
+function likedMoviesList() {
+  const item = JSON.parse(localStorage.getItem('liked_movies'));
+  let movies;
+
+  if (item) {
+    movies = item;
+  } else {
+    movies = {};
+  }
+
+  return movies;
+}
+
+function likeMovie(movie) {
+  const likedMovies = likedMoviesList();
+  
+  if (likedMovies[movie.id]) {
+    likedMovies[movie.id] = undefined;
+  } else {
+    likedMovies[movie.id] = movie;
+  }
+
+  localStorage.setItem('liked_movies', JSON.stringify(likedMovies));
+}
 
 // Utils
 
@@ -58,7 +84,7 @@ function createMovies(
     movieBtn.classList.add('movie-btn');
     movieBtn.addEventListener('click', () => {
       movieBtn.classList.toggle('movie-btn--liked');
-      // Deberiamos Agregar La Pelicula A LS
+      likeMovie(movie);
     })
 
     if (lazyload) {
